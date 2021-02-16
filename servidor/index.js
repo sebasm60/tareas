2b738ae05feb68bf837c6a7510f1d530a432c2fe
Sebastian Jimenez <sebasg1325@hotmail.com>
2021-02-16 12:12:06 -0500

    mm

--- servidor/index.js
+++ servidor/index.js
@@ -8,7 +8,7 @@ const app = express();
 require('./config/database');
 
 //Setings
-app.set('port', process.env.PORT || 3001);
+app.set('port', process.env.PORT || 3000);
 
 //middwares
 app.use(morgan('dev'));
@@ -16,7 +16,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(multer({dest : path.join(__dirname,'/img/uploads')}).single('imagen'));
-
 
 //Routes
 app.use('/api', require('./routes/routerTareas'));
